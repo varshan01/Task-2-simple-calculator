e5e3a3c5d500f800558f1d5b21c582f9d024ddfa
varshan01 <63407283+varshan01@users.noreply.github.com>
2024-07-09 12:56:00 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+    double num1, num2, result;
+
+    cout << "Simple Calculator" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    while (choice!= 5) {
+        switch (choice) {
+            case 1:
+                cout << "Enter two numbers: ";
+                cin >> num1 >> num2;
+                result = num1 + num2;
+                cout << "Result: " << result << endl;
+                break;
+            case 2:
+                cout << "Enter two numbers: ";
+                cin >> num1 >> num2;
+                result = num1 - num2;
+                cout << "Result: " << result << endl;
+                break;
+            case 3:
+                cout << "Enter two numbers: ";
+                cin >> num1 >> num2;
+                result = num1 * num2;
+                cout << "Result: " << result << endl;
+                break;
+            case 4:
+                cout << "Enter two numbers: ";
+                cin >> num1 >> num2;
+                if (num2!= 0) {
+                    result = num1 / num2;
+                    cout << "Result: " << result << endl;
+                } else {
+                    cout << "Error: Division by zero!" << endl;
+                }
+                break;
+            default:
+                cout << "Invalid choice. Try again!" << endl;
+        }
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+    }
+
+    return 0;
+}
